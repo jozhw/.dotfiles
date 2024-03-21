b2ee8e4851d373568cbb7322d9a49882e288b14e
John W. <112655410+jozhw@users.noreply.github.com>
2024-03-21 12:44:03 -0500

    update-nvim: latex template path from ~/latex-templates/... to ~/templates/latex/...

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -11,19 +11,19 @@ local keymap = vim.keymap -- for conciseness
 keymap.set(
     "n",
     "<localleader>tje",
-    "<cmd>read ~/latex-templates/journal-entry/JournalEntry.tex<CR>"
+    "<cmd>read ~/templates/latex/journal-entry/JournalEntry.tex<CR>"
 )
 
 keymap.set(
     "n",
     "<localleader>tmn",
-    "<cmd>read ~/latex-templates/message-notes/MessageNotes.tex<CR>"
+    "<cmd>read ~/templates/latex/message-notes/MessageNotes.tex<CR>"
 )
 
 keymap.set(
     "n",
     "<localleader>tba",
-    "<cmd>read ~/latex-templates/book-analysis/BookAnalysis.tex<CR>"
+    "<cmd>read ~/templates/latex/book-analysis/BookAnalysis.tex<CR>"
 )
 local config = require("nvim-surround.config")
 
