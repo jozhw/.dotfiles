17eee5fb9ca5c939c840d5686e00b0af730d8b52
John W. <112655410+jozhw@users.noreply.github.com>
2024-04-15 09:27:32 -0500

    archive: projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -384,28 +384,6 @@ folder, otherwise delete a word"
 (with-eval-after-load 'eglot
    (setq completion-category-defaults nil))
 
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder where you keep your Git repos!
-  (when (file-directory-p "~/projects-code")
-    (setq projectile-project-search-path '("~/projects-code")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-(jw/leader-key-def
-  "pf"  'projectile-find-file
-  "ps"  'projectile-switch-project
-  "pp"  'projectile-find-file
-  "pc"  'projectile-compile-project
-  "pd"  'projectile-dired)
-
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
@@ -435,3 +413,25 @@ folder, otherwise delete a word"
   :ensure t
   :config
   (global-evil-surround-mode 1))
+
+;; (use-package projectile
+;;   :diminish projectile-mode
+;;   :config (projectile-mode)
+;;   :custom ((projectile-completion-system 'ivy))
+;;   :bind-keymap
+;;   ("C-c p" . projectile-command-map)
+;;   :init
+;;   ;; NOTE: Set this to the folder where you keep your Git repos!
+;;   (when (file-directory-p "~/projects-code")
+;;     (setq projectile-project-search-path '("~/projects-code")))
+;;   (setq projectile-switch-project-action #'projectile-dired))
+
+;; (use-package counsel-projectile
+;;   :config (counsel-projectile-mode))
+
+;; (jw/leader-key-def
+;;   "pf"  'projectile-find-file
+;;   "ps"  'projectile-switch-project
+;;   "pp"  'projectile-find-file
+;;   "pc"  'projectile-compile-project
+;;   "pd"  'projectile-dired)
