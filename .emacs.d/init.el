8240f5f1539d3b48c105f74286f3f53b66782a55
John W. <112655410+jozhw@users.noreply.github.com>
2024-04-22 12:20:50 -0500

    init-emacs: org-roam with org-roam-ui

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -381,6 +381,40 @@ folder, otherwise delete a word"
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'jw/org-babel-tangle-config)))
 
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/OrgRoamTest/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+(setq zettlekasten-paths-alist '(("Main" . "~/Trove/Epektasi/")
+                                 ("Zettlekasten" . "~/Trove/Zettlekasten/")
+                                 ("Test" . "~/OrgRoamTest/")))
+
+(defun switch-zettlekasten ()
+  (interactive)
+  (let* ((keys (mapcar #'car zettlekasten-paths-alist))
+         (prompt (format "Select Zettlekasten:"))
+         (key (completing-read prompt keys))
+         (chosen-zettlekasten-path (cdr (assoc key zettlekasten-paths-alist))))
+    (setq org-roam-directory chosen-zettlekasten-path)
+    (setq org-roam-db-location (concat chosen-zettlekasten-path "org-roam.db"))
+    (org-roam-db-sync)))
+
+(package-install 'org-roam-ui)
+
 (with-eval-after-load 'eglot
    (setq completion-category-defaults nil))
 
