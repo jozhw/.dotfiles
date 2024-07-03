5389205e87f17f247b5af44b7980a80d75062df5
John W. <112655410+jozhw@users.noreply.github.com>
2024-07-03 14:14:04 -0500

    fix-archive: emacs archived packages with wrong file name

--- archive/emacs/org-roam.el
+++ archive/emacs/org-roam.el
@@ -15,3 +15,17 @@
   (org-roam-db-autosync-mode)
   If using org-roam-protocol
   (require 'org-roam-protocol))
+
+(setq zettlekasten-paths-alist '(("Main" . "~/Trove/Epektasi/")
+                                 ("Zettlekasten" . "~/Trove/Zettlekasten/")
+                                 ("Test" . "~/OrgRoamTest/")))
+
+(defun switch-zettlekasten ()
+  (interactive)
+  (let* ((keys (mapcar #'car zettlekasten-paths-alist))
+         (prompt (format "Select Zettlekasten:"))
+         (key (completing-read prompt keys))
+         (chosen-zettlekasten-path (cdr (assoc key zettlekasten-paths-alist))))
+    (setq org-roam-directory chosen-zettlekasten-path)
+    (setq org-roam-db-location (concat chosen-zettlekasten-path "org-roam.db"))
+    (org-roam-db-sync)))
