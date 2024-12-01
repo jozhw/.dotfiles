87601f424e85cd1c802c13240ffb96fbc6d6c9eb
John W <j_zhw@proton.me>
2024-11-30 22:04:01 -0600

    add: pyright for python-black lsp

--- .config/langs/py/lsp.sh
+++ .config/langs/py/lsp.sh
@@ -6,8 +6,12 @@ conda init
 
 conda deactivate
 
-conda install conda-forge::pyright
-
 conda activate base
 
+conda install conda-forge::pyright
+
 which pyright
+
+# install black-macchiato for python partial formatting with python-black
+
+pip install black-macchiato
