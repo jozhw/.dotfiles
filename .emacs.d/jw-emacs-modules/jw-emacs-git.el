58e10693383fe2d8cd834c9f78c68eeac46faab8
John W <j_zhw@proton.me>
2024-11-30 22:01:45 -0600

    emacs-update: forge

--- .emacs.d/jw-emacs-modules/jw-emacs-git.el
+++ .emacs.d/jw-emacs-modules/jw-emacs-git.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
+(setq forge-add-default-bindings nil)
+
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
@@ -9,6 +11,13 @@
 ;; NOTE: Make sure to configure a GitHub token before using this package!
 ;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
 ;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
-(use-package forge)
+(use-package forge
+  :after magit
+  )
+(use-package evil-collection
+  :after (evil forge)
+  :config
+  (evil-collection-init)
+  (evil-collection-forge-setup))
 
 (provide 'jw-emacs-git)
