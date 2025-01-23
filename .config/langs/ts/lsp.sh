df61f385b0da2b698b09e3f6236b86948218a085
John W <j_zhw@proton.me>
2025-01-23 00:27:43 -0600

    add: ts necessary installations for lsp support

---
+++ .config/langs/ts/lsp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install typescript language server
+npm install -g typescript-language-server typescript
+
+# install eslint plugin
+npm install typescript-eslint-language-service -D
+
+# install prettier for ts code formatting
+npm install -g prettier
+
