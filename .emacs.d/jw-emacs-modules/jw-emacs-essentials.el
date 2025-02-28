9dcf8ef3902a3c31042371a272e8c2ab5cb47918
John W <j_zhw@proton.me>
2025-02-28 17:19:51 -0500

    add/emacs: window switching bindings

--- .emacs.d/jw-emacs-modules/jw-emacs-essentials.el
+++ .emacs.d/jw-emacs-modules/jw-emacs-essentials.el
@@ -14,4 +14,9 @@
 
 (setq-default fill-column 80)
 
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+
 (provide 'jw-emacs-essentials)
