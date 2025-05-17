24e6e10a672b4372491acdcd1d1737e020108ab2
John W <j_zhw@proton.me>
2025-05-16 20:37:12 -0400

    refactor/brew: make sure all brew related is in the brew scripts dir

--- .config/brew/scripts/brew_langs.sh
+++ .config/brew/scripts/brew_langs.sh
@@ -1,18 +1,44 @@
 #!/usr/bin/env bash
 
+######################################################################################
+### Install programming languages
 
-# Install programming languages
-brew install lua  # Powerful, efficient, lightweight, embeddable scripting language
-#brew install php  # PHP programming language
 
-# Install lsp
-brew install rust-analyzer
+brew install lua 
 
-# Install r
+# brew install php  # PHP programming language
+
 brew install r
 
-# Install texlive for latex
+# for latex
 brew install texlive
+
+
+
+#####################################################################################
+### lsp
+
+
+# Install lsp for rust
+brew install rust-analyzer
 
 # Install clang support
 brew install llvm
+# download clang formatter
+brew install clang-format
+
+# Install for md support
+brew install marksman
+
+# install pyright (lsp) using conda forget to have it in .../miniconda3/bin/pyright
+# Install miniconda for python envs.
+brew install --cask miniconda
+conda init # make sure that conda is installed before running this
+conda deactivate
+conda activate base
+conda install conda-forge::pyright
+which pyright
+# for formatting does not work with conda for some reason
+brew install black
+# install black-macchiato for python partial formatting with python-black
+
--- .config/langs/clang/cpp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# download formatter
-brew install clang-format
