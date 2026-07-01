7ff3157a41730dd4d5b07c844b23d4d0512782b8
jozhw <j_zhw@proton.me>
2026-07-01 14:56:33 -0500

    rm: unused brew cask packages

--- .config/brew/scripts/brew_cask.sh
+++ .config/brew/scripts/brew_cask.sh
@@ -2,26 +2,21 @@
 
 # Install command-line tools using Homebrew with --cask.
 
-# Install fastmail client -- as of 20250515 migrated fully to protonmail
-# brew install --cask fmail2
 
 # Install emacs with GUI
 brew install --cask emacs
 
+# Install zen browser
+brew install --cask zen
+
+# Install arc browser
+brew install --cask arc
+
+# Install positron... better rstudio
+brew install --cask positron
+
 # Install macfuse to extend file handling capabilities
 brew install --cask macfuse
-
-# Install inkscape for photo editing
-brew install --cask inkscape
-
-# Install gnucash for financial management
-# brew install --cask gnucash # dont really use
-
-# Install mullvadvpn
-# brew install --cask mullvadvpn # sticking with tailscale mullvadvpn for convenience
-
-# Install rstudio
-brew install --cask rstudio
 
 # Install tor browser
 brew install --cask tor-browser
@@ -44,16 +39,7 @@ brew install --cask mkvtoolnix
 # Install audacity audio recording:::Avoid due to privacy concerns and controversies
 #brew install --cask audacity
 
-# Install Frappe Books an accounting software
-brew install --cask frappe-books
-
 # Install Ghostty a native terminal emulator
 brew install --cask ghostty
-
-# Install email client
-# brew install --cask thunderbird # just sticking with protonmail
-
-# Install LM Studio for locally hosted lms
-brew install --cask lm-studio
 
 brew install --cask skim
