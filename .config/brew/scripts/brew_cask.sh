9e70dbda6cc5408b962915269d16c4ceaeb9be7a
jozhw <j_zhw@proton.me>
2026-07-02 00:00:47 -0500

    update: add proton casks and rm old deprecated

--- .config/brew/scripts/brew_cask.sh
+++ .config/brew/scripts/brew_cask.sh
@@ -2,6 +2,11 @@
 
 # Install command-line tools using Homebrew with --cask.
 
+# password manager
+brew install --cask proton-pass
+
+# proton mail
+brew install --cask proton-mail
 
 # Install emacs with GUI
 brew install --cask emacs
@@ -27,17 +32,8 @@ brew install --cask qbittorrent
 # Install vlc media player
 brew install --cask vlc
 
-# Install discord
-brew install --cask discord
-
 # Install docker
 brew install --cask docker
-
-# Install mkv editor
-brew install --cask mkvtoolnix
-
-# Install audacity audio recording:::Avoid due to privacy concerns and controversies
-#brew install --cask audacity
 
 # Install Ghostty a native terminal emulator
 brew install --cask ghostty
